2077bf487f51ecb7a40a126d9b3d03b9d0b4d731
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2023-10-03 18:05:49 -0700

    create new account

--- Final.py
+++ Final.py
@@ -1,4 +1,7 @@
 # Function to load data from data.txt and return a list of users
+from secrets import choice
+
+
 def load_data(file_name):
     users = []
     with open(file_name, 'r') as file:
@@ -31,3 +34,50 @@ def withdraw(users, username, password, amount):
             return False
     else:
         return False
+
+def transfer(users, sender_username, sender_password, amount, receiver_username):
+    sender = login(users, sender_username, sender_password)
+    receiver = None
+    if sender:
+        for user in users:
+            if user['username'] == receiver_username:
+                receiver = user
+                break
+        if receiver:
+            if sender['balance'] >= amount:
+                sender['balance'] -= amount
+                receiver['balance'] += amount
+                return True
+            else:
+                print("Insufficient balance")
+                return False
+    return False
+
+#new account
+def create_account(users, new_username, new_password, full_name):
+    for user in users:
+        if user['username'] == new_username:
+            print("Username already exists. Please choose a different username.")
+            return False
+    users.append({
+        'username': new_username,
+        'password': new_password,
+        'full_name': full_name,
+        'balance': 0.0
+    })
+    with open("data.txt", 'a') as file:
+        file.write(f"{new_username},{new_password},{full_name},0.0\n")
+    return True
+
+ if __name__ == "__main__":
+    data_file = "data.txt"
+    users_data = load_data(data_file)
+
+    while True:
+        print("1. Login")
+        print("2. Accrue Interest")
+        print("3. Deposit")
+        print("4. Withdraw")
+        print("5. Transfer")
+        print("6. Create Account")
+        choice = input("Select an Option: ")
